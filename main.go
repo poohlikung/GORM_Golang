243e1c5ffa4c7581c1752f07a31d7d1fd7258228
PoohSorawit <hillingstudio@gmail.com>
2026-06-26 12:13:48 +0700

    add model.go

--- main.go
+++ main.go
@@ -8,20 +8,12 @@ import (
 )
 
 const (
-	host     = "postgres"   // or the Docker service name if running in another container
+	host     = "localhost"  // or the Docker service name if running in another container
 	port     = 5432         // default PostgreSQL port
 	user     = "myuser"     // as defined in docker-compose.yml
 	password = "mypassword" // as defined in docker-compose.yml
 	dbname   = "mydatabase" // as defined in docker-compose.yml
 )
-
-type Book struct {
-	ID          int64
-	Name        string
-	Author      string
-	Description string
-	price       uint
-}
 
 func main() {
 	// Configure your PostgreSQL database details here
@@ -33,4 +25,6 @@ func main() {
 		panic("failed to connect to database")
 	}
 	fmt.Print(db)
+	db.AutoMigrate(&Book{})
+	// db.Migrator().DropColumn(&Book{}, "name")
 }
